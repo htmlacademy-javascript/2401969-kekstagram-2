f9fd449f8ce4a9ca0070e60d6ad2e9539082d66a
vikkont79 <vvikkont79@yandex.ru>
2024-11-26 12:54:36 +0300

    Добавил функции и файл prettierrc

---
+++ js/function.js
@@ -0,0 +1,58 @@
+// Проверка на максимальную длину строки
+
+const checkLength = (string, maxLength) => string.length <= maxLength;
+
+console.log(checkLength('abracadabra', 15));
+
+// Проверка на палиндром с использованием цикла
+
+const checkPalindrom = (string) => {
+  const normilizedString = string.toLowerCase().replaceAll(' ', '');
+  let reverseString = '';
+  for (let i = normilizedString.length - 1; i >= 0; i--) {
+    reverseString += normilizedString[i];
+  }
+  return normilizedString === reverseString;
+};
+
+console.log(checkPalindrom('Аргентина манит негра'));
+
+// Проверка на палиндром с использований методов объектов
+
+const isPalindrom = (string) => {
+  const normilizedString = string.toLowerCase().replaceAll(' ', '');
+  const reverseString = normilizedString.split('').reverse().join('');
+  return normilizedString === reverseString;
+};
+
+console.log(isPalindrom('Кекс'));
+
+// Извлечение чисел из строки с использованием цикла
+
+const isNumber = (string) => {
+  const normilizedString = string.toString().replaceAll(' ', '');
+  let resultString = '';
+  for (let i = 0; i < normilizedString.length; i++) {
+    if (!isNaN(normilizedString[i])) {
+      resultString += normilizedString[i];
+    }
+  }
+  return parseInt(resultString, 10);
+};
+
+console.log(isNumber('1 литр рома на 1,5 литра колы'));
+
+// Извлечение чисел из строки через преобразование строки в объект
+
+const extractNumber = (string) => {
+  const normilizedString = string.toString().split('');
+  let resultString = '';
+  for (let i = 0; i < normilizedString.length; i++) {
+    if (!isNaN(normilizedString[i]) && normilizedString[i] !== ' ') {
+      resultString += normilizedString[i];
+    }
+  }
+  return parseInt(resultString, 10);
+};
+
+console.log(extractNumber('2,5 грамма протеина на 1 кг массы тела'));
